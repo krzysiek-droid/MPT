b0efb2afe18e9c1c2818bf9bd685e4a581769e24
Krzysztof Fryzowicz <goldenbullet1142@gmail.com>
2022-01-17 23:44:16 +0100

    image analysis, contour found and acquired

--- img_processing.py
+++ img_processing.py
@@ -23,11 +23,11 @@ if __name__ == "__main__":
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     cv2.threshold(src=img, dst=img, thresh=5, maxval=255, type=cv2.THRESH_BINARY)
-    cv2.imwrite('processed_images/binaryzation.png', img)
+    cv2.imwrite(r'data/processed_images/binaryzation.png', img)
     dilated = cv2.dilate(img, (3, 3), iterations=1)
-    cv2.imwrite('processed_images/dilated.png', dilated)
+    cv2.imwrite(r'data/processed_images/dilated.png', dilated)
     eroded = cv2.erode(dilated, (3, 3), iterations=2)
-    cv2.imwrite('processed_images/eroded.png', eroded)
+    cv2.imwrite(r'data/processed_images/eroded.png', eroded)
 
 
     # sngl_pxls_removed = morphology.remove_small_objects(eroded.astype(bool))
